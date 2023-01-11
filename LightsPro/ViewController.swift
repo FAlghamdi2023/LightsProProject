faa6dc69139f30e8d87390de8d11a58547255333
Nayyar <nayyar@su.edu.sa>
2023-01-11 16:44:20 +0300

    Initial Commit

---
+++ LightsPro/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  LightsPro
+//
+//  Created by AATCe on 11/01/2023.
+//
+
+import UIKit
+class ViewController: UIViewController {
+    @IBOutlet weak var lightStatus: UIButton!
+    var LightOn = true          // Boolean Variable
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateColor()
+    }
+    func updateColor() {
+        if LightOn{
+            lightStatus.setTitle("On", for: .normal)
+            view.backgroundColor = .white
+        }
+        else{
+            view.backgroundColor = .black
+            lightStatus.setTitle("Off", for: .normal)
+        }
+    }
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        //print("Button pressed!")
+        LightOn.toggle()
+        updateColor()
+    }
+}
+
